60c6ec574917d2b8d5f3ea68cb865a8ce4e14821
MuhamadRizkiAkbar <146195546+MuhamadRizkiAkbar@users.noreply.github.com>
2024-01-19 09:01:39 +0600

    Update db.js

--- models/db.js
+++ models/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root', //sesuaikan dengan username mysql masing-masing
     password: '' , //sesuatkan dengan password mysql masing-masing
-    database: 'kuliah',
+    database: 'mahasiswa',
 
 });
 
